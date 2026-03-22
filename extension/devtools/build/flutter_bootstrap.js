550d7111b24590f0686f5f46a80897134d868878
Elias <elias@deuss.dev>
2026-03-22 11:10:36 +0100

    chore: rebuild extension

--- extension/devtools/build/flutter_bootstrap.js
+++ extension/devtools/build/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3374497645" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2757771356" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
